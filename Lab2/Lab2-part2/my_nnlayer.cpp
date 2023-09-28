0191fe626aba37766e4d45772213a11437488f58
Adam Jesse <adam.jesse23@gmail.com>
2023-09-28 15:06:34 -0400

    Part 2 complete

--- Lab2/Lab2-part2/my_nnlayer.cpp
+++ Lab2/Lab2-part2/my_nnlayer.cpp
@@ -5,15 +5,14 @@ using namespace std;
 class Network_Layex {
 public:
     double *a;   // Pointer to input values
-    double *w;   // Pointer to weights
+    double **w;   // Pointer to weights
     double *b;    // Bias
     double *z;    //membrane potential
 
     size_t NI;   // Number of inputs
     size_t NO;   // Number of outpus
     size_t af;   // Activation function (1: ReLU, 2: tanh)
-    double z;    // Weighted sum
-    double a_o;  // Output after activation
+    double *a_o;  // Output after activation
 
     // Constructors
     Network_Layex () { // Default constructor
@@ -28,49 +27,88 @@ public:
     //     delete[] w; // Free allocated memory for weights
     // }
 
-    void operator() (double *a_i, double *w_i,double *b_i,size_t af_i)
+    void operator() (double *a_i, double **w_i,double *b_i,size_t af_i)
     {
-        a = a_i; w = w_i; b = b_i; af = af_i;
-        z = 0; 
-        for(int i = 0; i < NI; i++)
-        {
-            z = z + a[i]*w[i];
-            z = z+b[i];
-
-            //apply non-linear operation
-            if (af == 1)
-            { if(z>=0) a_o=z; else a_o =0;}
-            else if(af == 2)
-                {a_o = tanh(z);}
-            else 
-                {cout << "error"; a_o = z;}
+        a = a_i; w = w_i; b = b_i; af = af_i; 
+        z = (double *) calloc (NO, sizeof(double));
+        a_o= (double *) calloc (NO, sizeof(double));
+        
+        for(int i = 0; i < NO; i++)
+        { z[i] = 0;
+            for(int j = 0; j < NI; j++)
+            {
+                z[i] += a[j]*w[i][j];
             }
-
+            z[i] += b[i];
         }
+            
 
+        //apply non-linear operation
+        if (af == 1)
+        { 
+            for(int i = 0; i < NO; i++)
+            {
+                if(z[i]>=0) {a_o[i]=z[i];}
+                else {a_o[i] =0;}
+            }
+        }
+        else if(af == 2)
+            for(int i = 0; i < NO; i++)
+            {
+                a_o[i] = tanh(z[i]);
+            }
+        else 
+            {cout << "error";}
+        };
+        void result() 
+        {
+        int i;
+        for (i = 0; i < NO; i++)
+        {
+        cout << "Membrane potential (z): " << z[i] << endl;
+        }
+        for (i = 0; i < NO; i++)
+        {
+        cout << "Action potential (a_o): " << a_o[i] << endl; 
+        }
+        };
 
-    };
+        };
 
 int main()
 {   
     size_t NI = 5;
     size_t NO = 4;
-    double *a,*w,*b;
+    double *a,*b;
+    double** w;
     int af = 1;
 
     Network_Layex NL(NI,NO);
 
+    // a = new double[NI];
+    // b = new double[NI];
+    // w = new double*[NO];
     a = (double *) calloc (NI, sizeof(double));
-    w = (double *) calloc (NI*NO, sizeof(double));
+    w = (double **) calloc (NO, sizeof(double*));
+    for(int i = 0; i < NO; ++i)
+    {
+        w[i] = (double*)calloc(NI,sizeof(double));
+    }
     b = (double *) calloc (NI, sizeof(double));
-    if(a == NULL) return 0;
-    if(w == NULL) return 0;
+    // if(a == NULL) return 0;
+    // if(w == NULL) return 0;
 
 
-    w[4][5] =       {{ 0.25,  0.50, -3.20, -4.50, -2.00},
-                     { 2.00,  3.25,  5.75,  6.25,  7.15},
-                     { 0.25, -3.50,  0.25,  0.25,  0.25},
-                     { 2.00,  3.25,  0.75, -6.50,  1.50}};
+    // {{ 0.25,  0.50, -3.20, -4.50, -2.00},
+    // { 2.00,  3.25,  5.75,  6.25,  7.15},
+    // { 0.25, -3.50,  0.25,  0.25,  0.25},
+    // { 2.00,  3.25,  0.75, -6.50,  1.50}};
+
+    w[0][0] = 0.25; w[0][1] =0.5; w[0][2] = -3.20; w[0][3] = -4.50; w[0][4] = -2.00;
+    w[1][0] = 2.00; w[1][1] =3.25; w[1][2] = 5.75; w[1][3] = 6.25;  w[1][4] = 7.15;
+    w[2][0] = 0.25; w[2][1] =-3.50;w[2][2] = 0.25;w[2][3] = 0.25; w[2][4] = 0.25; 
+    w[3][0] = 2.00; w[3][1] =3.25; w[3][2] = 0.75; w[3][3] = -6.50; w[3][4] = 1.50;
+
 
     a[0] = 2.5; a[1] = 3.0; a[2] = 2.5; a[3] = 1.5; a[4] = 1.0;
  
@@ -78,8 +116,23 @@ int main()
  
 
     NL(a,w,b,af);
+    NL.result();
 
-
-    free(a); free(w); free(b);
-
+    // free(a);  free(b);
+    // for(int i = 0; i < NO; i++)
+    // {
+    //     delete[] w[i];
+    // }
+    // delete[] w;
+    // delete[] a;
+    // delete[] b;
+    for(int i = 0; i < NO; ++i)
+    {
+        free(w[i]);
+    }
+    free(w);
+    free(a);
+    free(b);
+    
+    return 0;
 }
