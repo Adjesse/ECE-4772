bec1056fcb8c11e5072f005b546395054c2bf9ac
Adam Jesse <adam.jesse23@gmail.com>
2023-09-28 09:43:10 -0400

    Changed File Structure

---
+++ Lab2/Lab2-part2/my_nnlayer.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class Neuron {
+    public:
+        double *a;
+        double *w;
+        double b;
+
+        size_t NI;
+        size_t af;
+        double z;
+        double a_o;
+
+        //Constuctor 
+        Neuron():{NI = 5; af = 1};
+        
+
+
+}
